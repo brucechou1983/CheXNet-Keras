84c3679bc421368608f37a4f5960cce95a32cb2e
Tao-Yi Lee <tylee@ieee.org>
2018-01-29 12:37:40 +0800

    add CAM capability

---
+++ grad_cam.py
@@ -0,0 +1,125 @@
+from keras.applications.vgg16 import (
+    VGG16, preprocess_input, decode_predictions)
+from keras.preprocessing import image
+from keras.layers.core import Lambda
+from keras.models import Sequential, Model
+from tensorflow.python.framework import ops
+import keras.backend as K
+import tensorflow as tf
+import numpy as np
+import keras
+import sys
+import cv2
+
+def target_category_loss(x, category_index, nb_classes):
+    return tf.multiply(x, K.one_hot([category_index], nb_classes))
+
+def target_category_loss_output_shape(input_shape):
+    return input_shape
+
+def normalize(x):
+    # utility function to normalize a tensor by its L2 norm
+    return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
+
+def load_image(path):
+    img_path = sys.argv[1]
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+def register_gradient():
+    if "GuidedBackProp" not in ops._gradient_registry._registry:
+        @ops.RegisterGradient("GuidedBackProp")
+        def _GuidedBackProp(op, grad):
+            dtype = op.inputs[0].dtype
+            return grad * tf.cast(grad > 0., dtype) * \
+                tf.cast(op.inputs[0] > 0., dtype)
+
+def compile_saliency_function(model, activation_layer='block5_conv3'):
+    input_img = model.input
+    layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
+    layer_output = layer_dict[activation_layer].output
+    max_output = K.max(layer_output, axis=3)
+    saliency = K.gradients(K.sum(max_output), input_img)[0]
+    return K.function([input_img, K.learning_phase()], [saliency])
+
+def modify_backprop(model, name):
+    g = tf.get_default_graph()
+    with g.gradient_override_map({'Relu': name}):
+
+        # get layers that have an activation
+        layer_dict = [layer for layer in model.layers[1:]
+                      if hasattr(layer, 'activation')]
+
+        # replace relu activation
+        for layer in layer_dict:
+            if layer.activation == keras.activations.relu:
+                layer.activation = tf.nn.relu
+
+        # re-instanciate a new model
+        new_model = VGG16(weights='imagenet')
+    return new_model
+
+def deprocess_image(x):
+    '''
+    Same normalization as in:
+    https://github.com/fchollet/keras/blob/master/examples/conv_filter_visualization.py
+    '''
+    if np.ndim(x) > 3:
+        x = np.squeeze(x)
+    # normalize tensor: center on 0., ensure std is 0.1
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+
+    # clip to [0, 1]
+    x += 0.5
+    x = np.clip(x, 0, 1)
+
+    # convert to RGB array
+    x *= 255
+    if K.image_dim_ordering() == 'th':
+        x = x.transpose((1, 2, 0))
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+def grad_cam(input_model, image, category_index, layer_name):
+    output = input_model.output
+
+    nb_classes = 2
+    target_layer = lambda x: target_category_loss(x, category_index, nb_classes)
+    final_layer = Lambda(target_layer,
+                     output_shape = target_category_loss_output_shape)
+    output = final_layer(output)
+    model = Model(inputs=input_model.input, outputs=output)
+    loss = K.sum(model.layers[-1].output)
+    conv_output =  model.get_layer(layer_name).output
+    grads = normalize(K.gradients(loss, conv_output)[0])
+    print("grads tensor = {}".format(grads))
+    gradient_function = K.function([model.layers[0].input, K.learning_phase()], [conv_output, grads])
+
+    output, grads_val = gradient_function([image, 0])
+    print("output = {}, grads_val = {}".format(np.shape(output), np.shape(grads_val)))
+    output, grads_val = output[0, :], grads_val[0, :, :, :]
+
+    weights = np.mean(grads_val, axis = (0, 1))
+    cam = np.ones(output.shape[0 : 2], dtype = np.float32)
+
+    for i, w in enumerate(weights):
+        cam += w * output[:, :, i]
+
+    cam = cv2.resize(cam, (224, 224))
+    cam = np.maximum(cam, 0)
+    heatmap = cam / np.max(cam)
+
+    #Return to BGR [0..255] from the preprocessed image
+    image = image[0, :]
+    image -= np.min(image)
+    image = np.minimum(image, 255)
+
+    cam = cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
+    cam = np.float32(cam) + np.float32(image)
+    cam = 255 * cam / np.max(cam)
+    return np.uint8(cam), heatmap
---
+++ test_cam.py
@@ -0,0 +1,93 @@
+import numpy as np
+import os
+from callback import load_generator_data
+from configparser import ConfigParser
+from generator import custom_image_generator
+from models.densenet121 import get_model
+from utility import get_sample_counts
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import roc_auc_score
+
+
+def main():
+    # parser config
+    config_file = "./config.ini"
+    cp = ConfigParser()
+    cp.read(config_file)
+
+    # default config
+    output_dir = cp["DEFAULT"].get("output_dir")
+    class_names = cp["DEFAULT"].get("class_names").split(",")
+
+    # test config
+    batch_size = cp["TEST"].getint("batch_size")
+    use_best_weights = cp["TEST"].getboolean("use_best_weights")
+
+    # parse weights file path
+    output_weights_name = cp["TRAIN"].get("output_weights_name")
+    weights_path = os.path.join(output_dir, output_weights_name)
+    best_weights_path = os.path.join(output_dir, f"best_{output_weights_name}")
+
+    # get test sample count
+    test_counts, _ = get_sample_counts(output_dir, "test", class_names)
+
+    symlink_dir_name = "image_links"
+    test_data_path = f"{output_dir}/{symlink_dir_name}/test/"
+
+    step_test = int(test_counts / batch_size)
+    print("** load test generator **")
+    test_generator = custom_image_generator(
+        ImageDataGenerator(horizontal_flip=True, rescale=1./255),
+        test_data_path,
+        batch_size=batch_size,
+        class_names=class_names,
+    )
+    x, y = load_generator_data(test_generator, step_test, len(class_names))
+
+    print("** load model **")
+    model = get_model(class_names)
+    if use_best_weights:
+        print("** use best weights **")
+        model.load_weights(best_weights_path)
+    else:
+        print("** use last weights **")
+        model.load_weights(weights_path)
+
+    print("** make prediction **")
+    y_hat = model.predict(x)
+
+    print("** perform grad cam **")
+    print("y_hat = {}".format(y_hat))
+    print("shape y_hat = {}".format(np.shape(y_hat)))
+    class_prob = [y_hat[i][:, 0] for i, class_name in enumerate(class_names)]
+    print("fx_prob = {}".format(np.shape(fx_prob)))
+    with open('predicted_class.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csvwriter.writerow(['ID'] + class_names)
+        for i in range(len(fx_prob)):
+            csvwriter.writerow([i] + class_prob[:, i])
+            xi_orig = 255*x_orig[i, :]
+            cv2.imwrite(f"imgdir/orig_image_{i}.jpg", np.uint8(xi_orig))
+            predicted_class = 0 if fx_prob[i] > nm_prob[i] else 1
+            cam, heatmap = gc.grad_cam(model, x[np.newaxis, i, :], predicted_class, "conv5_blk_scale")
+            cv2.imwrite(f"imgdir/gradcam.jpg_{i}", cam)
+            cv2.imwrite(f"imgdir/concat_gradcam_{i}.jpg", np.concatenate((np.uint8(xi_orig), cam), axis=1))
+
+    test_log_path = os.path.join(output_dir, "test.log")
+    print(f"** write log to {test_log_path} **")
+    aurocs = []
+    with open(test_log_path, "w") as f:
+        for i in range(len(class_names)):
+            try:
+                score = roc_auc_score(y[i], y_hat[i])
+                aurocs.append(score)
+            except ValueError:
+                score = 0
+            f.write(f"{class_names[i]}: {score}\n")
+        mean_auroc = np.mean(aurocs)
+        f.write("-------------------------\n")
+        f.write(f"mean auroc: {mean_auroc}\n")
+
+
+if __name__ == "__main__":
+    main()
